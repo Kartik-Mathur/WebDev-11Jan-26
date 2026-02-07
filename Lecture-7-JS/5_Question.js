3006cf61f7e93630ec0d837bc7809ca92e1ff466
Kartik <kartik.mathur10@gmail.com>
2026-02-07 18:55:00 +0530

    Array to Object-Question

--- Lecture-7-JS/5_Question.js
+++ Lecture-7-JS/5_Question.js
@@ -2,12 +2,13 @@ let url = [
     'http://codingblocks.com/courses/cpp',
     'http://codingblocks.com/courses/java',
     'http://codingblocks.com/courses/python',
-    'http://codingblocks.com/mentor/abhishek',
-    'http://codingblocks.com/mentor/mosina',
-    'http://codingblocks.com/mentor/kartik',
+    'http://codingblocks.com/mentors/abhishek',
+    'http://codingblocks.com/mentors/mosina',
+    'http://codingblocks.com/mentors/kartik',
 ]
 
 /* 
+    OUTPUT REQUIRED:
     {
         "codingblocks":{
             "courses": ['cpp', 'java', 'python'],
@@ -15,3 +16,32 @@ let url = [
         }
     }
 */
+let y = {};
+
+for (let i = 0; i < url.length; i++) {
+    let s = url[i];
+
+    let a = s.split('/');
+    // console.log(a);
+
+    let b = a[2].split('.');
+    let k = b[0];
+
+    if (!y[k]) {
+        y[k] = {}
+    }
+
+    let category = a[3];
+    let item = a[4];
+
+    if (!y[k][category]) {
+        y[k][category] = []
+    }
+
+    y[k][category].push(item);
+}
+
+
+console.log(y)
+console.log(y["codingblocks"]['courses']);
+console.log(y["codingblocks"]['mentors']);
