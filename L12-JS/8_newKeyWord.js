3cf6d7a50d9e5adcb1854726e3207f349d081a82
Kartik <kartik@codingblocks.com>
2026-02-23 09:31:10 +0530

    Promises and Bindings

---
+++ L12-JS/8_newKeyWord.js
@@ -0,0 +1,10 @@
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+let ankit = new Person("Ankit", 18);
+let Shaurya = new Person("Shaurya", 19);
+
+console.log(ankit)
+console.log(Shaurya)
