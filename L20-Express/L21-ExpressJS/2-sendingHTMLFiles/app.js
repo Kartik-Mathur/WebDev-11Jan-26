2d1b25b015ac906ccd79be1ad22ed1eee1623120
Kartik <kartik@codingblocks.com>
2026-04-11 17:21:54 +0530

    Express

--- L20-Express/L21-ExpressJS/2-sendingHTMLFiles/app.js
+++ L20-Express/L21-ExpressJS/2-sendingHTMLFiles/app.js
@@ -6,10 +6,21 @@ app.get('/',(req,res)=>{
     res.send("Hello World");
 })
 
+// IMPORTANT:
+// linking css,js or any other file in HTML is a separate request to the
+// server
 // To send HTML page we use sendFile function
 app.get('/page',(req,res)=>{
     // console.log(__dirname);
     res.sendFile(path.join(__dirname,'index.html'));
+})
+
+app.get('/style.css',(req,res)=>{
+    res.sendFile(path.join(__dirname,'style.css'));
+})
+
+app.get('/script.js',(req,res)=>{
+    res.sendFile(path.join(__dirname,'script.js'));
 })
 
 app.listen(4444, function(){
