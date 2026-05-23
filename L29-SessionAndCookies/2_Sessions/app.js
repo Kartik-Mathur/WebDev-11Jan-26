d9078767c73629ae95132c147832fb716318ca09
Kartik <kartik.mathur10@gmail.com>
2026-05-23 18:23:21 +0530

    Sessions

---
+++ L29-SessionAndCookies/2_Sessions/app.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const PORT = 4444;
+const session = require('express-session')
+app.use(session({
+  secret: 'akdsbb bdbasbaajhvejbfvhesjvfshjva mvesvvsecerjvsdsdvfkhsjfv',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/profile', (req, res) => {
+    if(!req.session.details){
+        return res.send("You need to login first");
+    }
+
+    req.session.details.visit++;
+
+    res.send(`Welcome back ${req.session.details.name} - ${req.session.details.visit}`);
+})
+
+app.get('/login', (req, res) => {
+    req.session.details = {
+        name: 'Kartik',
+        visit: 1
+    }
+
+    res.send("You are logged in");
+})
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:` + PORT);
+});
