f88fd5041f0505f1edb089b901cd34abb01da95d
Kartik <kartik.mathur10@gmail.com>
2026-05-23 17:26:14 +0530

    Cookiesg

--- L29-SessionAndCookies/1_Cookie/app.js
+++ L29-SessionAndCookies/1_Cookie/app.js
@@ -2,12 +2,36 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = 4444;
+const Cookies = require('cookies')
 
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/profile',(req,res)=>{
+    let cookies = new Cookies(req, res);
+    if(!cookies.get("details")){
+        return res.send("You need to login first");
+    }
+    let details = JSON.parse(cookies.get("details"));
+    
+    details.visit++;
+    cookies.set("details", JSON.stringify(details));
+    
+    res.send(`Welcome back ${details.name} - ${details.visit}`);
+})
 
+app.get('/login',(req,res)=>{
+    let cookies = new Cookies(req, res);
+    let details = {
+        name: "Kartik",
+        visit: 1
+    }
+    
+    cookies.set("details", JSON.stringify(details));
 
-app.listen(PORT,()=>{
-    console.log(`http://localhost:`+PORT);
+    res.send("You are logged in");
+})
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:` + PORT);
 });
