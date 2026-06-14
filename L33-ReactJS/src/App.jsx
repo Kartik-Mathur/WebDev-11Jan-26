9eda59be5207b14c3a0aae38aee10658c612792d
Kartik <kartik.mathur10@gmail.com>
2026-06-14 18:56:17 +0530

    Loops in react

--- L33-ReactJS/src/App.jsx
+++ L33-ReactJS/src/App.jsx
@@ -2,21 +2,30 @@ import React from "react";
 
 const App = () => {
   let movies = ["Avengers", "Hulk", "Ironman"];
- 
+  let movies1 = ["Avengers1", "Hulk1", "Ironman1"];
+  let movies2 = ["Avengers2", "Hulk2", "Ironman2"];
   return (
     <div>
       Movie List
-      <ul>
-        {movies.map((data, indx) => {
-          return <li key={indx} className="movie">{data}</li>;
-        })}
-      </ul>
-
-      <ul>
-        {movies.map((data, indx) => <li key={indx} className="movie">{data}</li>)}
-      </ul>
+      <Movies movies={movies} />
+      <Movies movies={movies1} />
+      <Movies movies={movies2} />
     </div>
   );
 };
+
+function Movies({movies}) {
+  return (
+    <ul>
+      {movies.map((data, indx) => {
+        return (
+          <li key={indx} className="movie">
+            {data}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
 
 export default App;
