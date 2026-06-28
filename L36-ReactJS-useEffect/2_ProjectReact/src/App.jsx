1a1332624fca1914e2e8c8ac8501bb05108caba5
Kartik <kartik.mathur10@gmail.com>
2026-06-28 17:35:47 +0530

    Increase priority

--- L36-ReactJS-useEffect/2_ProjectReact/src/App.jsx
+++ L36-ReactJS-useEffect/2_ProjectReact/src/App.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
+  const taskName = useRef();
+  const description = useRef();
 
   useEffect(() => {
     async function getTodos() {
@@ -18,16 +20,51 @@ const App = () => {
       data: { id },
     });
 
-    setTodos(data);
+    setTodos(data.allTodos);
   }
 
+  async function addTodoHandler(){
+    let taskValue = taskName.current.value.trim();
+    let descriptionValue = description.current.value.trim();
+
+    if(!taskValue || !descriptionValue) return;
+
+    let {data} = await axios.post('http://localhost:4444/todos',{
+      name: taskValue,
+      description: descriptionValue
+    })
+    
+    setTodos(data.allTodos);
+  }
+
+  async function upHandler(id, indx){
+    if(indx > 0){
+      let currentId = id;
+      let prevId = todos[indx - 1]._id;
+      let {data} = await axios.get('http://localhost:4444/increase-priority',{
+        params: {
+          prevId,
+          currentId
+        }
+      })
+      setTodos(data.allTodos);
+    }
+  }
   return (
     <div>
       My Todo App
+      <div>
+        <input ref={taskName} type="text" placeholder="Enter Task Name" />
+        <input ref={description} type="text" placeholder="Enter Description" />
+
+        <button onClick={addTodoHandler}>Add Todo</button>
+      </div>
       {todos.map((item, indx) => (
         <li key={indx}>
           {item.name} - {item.description}
           <button onClick={() => deleteHandler(item._id)}>Delete</button>
+          <button onClick={() => upHandler(item._id, indx)}>Up</button>
+          <button onClick={() => downHandler(item._id)}>Down</button>
         </li>
       ))}
     </div>
