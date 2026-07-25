c5e0b4409190cf77f43f85fe178c822d69806153
Kartik <kartik.mathur10@gmail.com>
2026-07-25 19:14:51 +0530

    Sockets

--- L41-Sockets/app.js
+++ L41-Sockets/app.js
@@ -18,20 +18,44 @@ app.get('/', (req, res) => {
 let eventsOn = false;
 
 let users = new Map();
+let allChats = [];
 
 io.on('connection', (socket) => {
 
     socket.on('join', ({ name }) => {
+        console.log("User name received", name);
         users.set(socket, name);
         socket.emit('welcome', {
             message: "Welcome to the chat",
+            activeUsers: users.size,
+            allChats
+        })
+
+        io.emit('update-user-count', {
             activeUsers: users.size
+        })
+    })
+
+
+    socket.on('chat-message', ({ message }) => {
+        console.log("Name" ,users.get(socket));
+        allChats.push({
+            user: users.get(socket),
+            message
+        })
+
+        socket.broadcast.emit('chat-received', {
+            message,
+            user: users.get(socket)
         })
     })
     // Handle disconnection
     socket.on('disconnect', () => {
         console.log('A user disconnected');
         users.delete(socket);
+        io.emit('update-user-count', {
+            activeUsers: users.size
+        })
     });
 });
 
